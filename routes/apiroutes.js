107bf5a867e07ffefa635147fd6967d1f581e203
zanatooo <m8elzanaty@gmail.com>
2022-05-17 21:51:14 -0700

    add apiroutes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,27 +1,43 @@
-const app=require ("express").Router()
+const app = require("express").Router()
 let db = require("../db/db.json");
 const fs = require("fs")
 
-app.get("/api/notes",function(res,res){
-   db = JSON.parse(fs.readFileSync("/db/db.json"))
-   console.log("get",db);
-   res.json(db)
+app.get("/api/notes", function (res, res) {
+    db = JSON.parse(fs.readFileSync("/db/db.json"))
+    console.log("get", db);
+    res.json(db)
 })
 
 
-app.post("/api/notes",function(res,res){
-    let data={
-        title:req.body.title,
-        text:req.body.text,
-        id: Math.floor(Math.random()*1009)
+app.post("/api/notes", function (res, res) {
+    let data = {
+        title: req.body.title,
+        text: req.body.text,
+        id: Math.floor(Math.random() * 1009)
     }
 
-db.push (data)
-fs.writeFileSync("/db/db.json",JSON.stringify(db),function(err,data){
-    if(err)throw err
+    db.push(data)
+    fs.writeFileSync("/db/db.json", JSON.stringify(db), function (err, data) {
+        if (err) throw err
+    })
+
+    console.log("post", db);
+    res.json(db)
 })
 
-    console.log("post",db);
+app.delet("/api/notes/:id", function (res, res) {
+    let tempdb;
+    for (let i = 0; i < db.length; i++) {
+        if (db[i].id != req.params.id) {
+            tempdb.push(db[i])
+        }
+    }
+    db=tempdb
+    fs.writeFileSync("/db/db.json", JSON.stringify(db), function (err, data) {
+        if (err) throw err
+    })
+
+    console.log("post", db);
     res.json(db)
- })
- 
+})
+module.exports=app 
